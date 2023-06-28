1229d69a51f64842462b4b9fe4a2395d99a9f8ee
unknown <soumyanayak963@gmail.com>
2023-06-28 19:42:12 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-from django.db.models.functions import Length
+from django.db.models.functions import Length #using lenth function
 
 from app.models import *
+
+from django.db.models import Q #using Q object for concatination
 # create new views for font end
 
 
@@ -20,7 +22,16 @@ def display_topic(request):
 #display Webpage Models
 
 def display_webpage(request):
-    webpages=Webpage.objects.filter(topic_name='Cricket')
+    # webpages=Webpage.objects.all()
+    # webpages=Webpage.objects.filter(name__startswith='V')
+    webpages=Webpage.objects.filter(url__startswith='http')
+    webpages=Webpage.objects.filter(name__endswith='i')
+    webpages=Webpage.objects.filter(url__contains='.com')
+    webpages=Webpage.objects.all()
+    webpages=Webpage.objects.filter(Q(name='Soumya') | Q(url='http:'))
+    webpages=Webpage.objects.filter(Q(name='Soumya') & Q(url='http://Soumya.com'))
+
+
     d={'webpages':webpages}
     return render(request,'display_webpage.html',d)
 
@@ -28,9 +39,51 @@ def display_webpage(request):
 
 def display_AcessRecord(request):
     Acess=AcessRecord.objects.all()
+    Acess=AcessRecord.objects.filter(date='2023-06-05')
+    Acess=AcessRecord.objects.filter(date__gt='2023-06-05')
+    Acess=AcessRecord.objects.filter(date__gte='2023-06-05')
+    Acess=AcessRecord.objects.filter(date__lt='2023-06-05')
+    Acess=AcessRecord.objects.filter(date__lte='2023-06-05')
+    Acess=AcessRecord.objects.filter(date__day__gte='05')
+    Acess=AcessRecord.objects.filter(date__year__lte='2023')
+    Acess=AcessRecord.objects.filter(date__month__lt='05')
+    Acess=AcessRecord.objects.all()
+
+
     d={'Acess':Acess}
     return render(request,'display_AcessRecord.html',d)
 
+
+
+def update_webpage(request):
+    # using update method
+    # Webpage.objects.filter(name='Soumya').update(url='https://SN.in')#single row certified then upadate
+    # Webpage.objects.filter(topic_name='Cricket').update(url='https://bcci.in') # ifmultiple row certisfied its update
+    # Webpage.objects.filter(topic_name='Cricket msd').update(url='https://bcci.in') #if Zero row certisfied not affected
+    # Webpage.objects.filter(topic_name='Cricket').update(url='https://bcci.in') #while fk column update we use value which is present in table
+
+# using update or create method
+    # Webpage.objects.update_or_create(name='Soumya',defaults={'url':'http://soumya.in'})#single row certified then upadate
+    # Webpage.objects.update_or_create(topic_name='Cricket',defaults={'url':'http://soumya.in'})#if multiple row certified it through error
+    # Webpage.objects.update_or_create(topic_name='Chess',defaults={'url':'http://s.in'})
+    CTO=Topic.objects.get(topic_name='Cricket')
+    Webpage.objects.update_or_create(name='pdya',defaults={'topic_name': CTO,})
+    #Webpage.objects.update_or_create(name='pandya',defaults={'topic_name': CTO,'url':'https://pandya.in'})#while fk column update we should provide objects which is present in table
+
+
+    webpages=Webpage.objects.all()
+    d={'webpages':webpages}
+    return render(request,'display_webpage.html',d)
+
+
+#delete the data
+def delete_webpage(request):
+    Webpage.objects.filter(name='pdya').delete()
+
+
+    webpages=Webpage.objects.all()
+    d={'webpages':webpages}
+    return render(request,'display_webpage.html',d)
 
 
 
--- project20/urls.py
+++ project20/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('display_topic/',display_topic,name='display_topic'),
     path('display_webpage/',display_webpage,name='display_webpage'),
     path('display_AcessRecord/',display_AcessRecord,name='display_AcessRecord'),
+    path('update_webpage/',update_webpage,name='update_webpage'),
+    path('delete_webpage/',delete_webpage,name='delete_webpage'),
 ]
